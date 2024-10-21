54b6ada873c07aef4d34a3659718655239acc709
Kennu156 <kenmarttipaju@gmail.com>
2024-10-21 13:20:19 +0300

    commit

--- index.php
+++ index.php
@@ -1,7 +1,45 @@
 <?php
 
+$host = 'd124550.mysql.zonevs.eu';
+$db   = 'd124550_books';
+$user = 'd124550_book';
+$pass = 'Books1133_';
+$charset = 'utf8mb4';
 
-$first = $_GET['first'];
-$last = $_GET['last'];
+$host = 'localhost';
+$db   = 'books';
+$user = 'root';
+$pass = '';
 
-echo "<h1>Hello {$first} {$last}!</h1>";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+$stmt = $pdo->query('SELECT * FROM books');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    while ($row = $stmt->fetch())
+    {
+        echo $row['title'] . "<br>";
+    }
+    
+    ?>
+
+</body>
+</html>
